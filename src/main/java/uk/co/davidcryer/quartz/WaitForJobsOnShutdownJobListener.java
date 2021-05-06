93d3e3ed39cdd6797c10a0c17fff8ca7dc9e9073
david.cryer <david.cryer@autotrader.co.uk>
2021-05-06 19:23:47 +0100

    remove atomic variables from shutdown job listener - they are being mutated inside synchronised blocks on the same lock, so I don't think they are necessary

--- src/main/java/uk/co/davidcryer/quartz/WaitForJobsOnShutdownJobListener.java
+++ src/main/java/uk/co/davidcryer/quartz/WaitForJobsOnShutdownJobListener.java
@@ -4,13 +4,10 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.quartz.listeners.JobListenerSupport;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class WaitForJobsOnShutdownJobListener extends JobListenerSupport {
     private final Object shutdownLock = new Object();
-    private final AtomicInteger runningJobs = new AtomicInteger(0);
-    private final AtomicBoolean shuttingDown = new AtomicBoolean(false);
+    private int runningJobs = 0;
+    private boolean shuttingDown = false;
 
     @Override
     public String getName() {
@@ -20,15 +17,15 @@ public class WaitForJobsOnShutdownJobListener extends JobListenerSupport {
     @Override
     public void jobToBeExecuted(JobExecutionContext context) {
         synchronized (shutdownLock) {
-            runningJobs.incrementAndGet();
+            ++runningJobs;
         }
     }
 
     @Override
     public void jobWasExecuted(JobExecutionContext context, JobExecutionException jobException) {
         synchronized (shutdownLock) {
-            runningJobs.decrementAndGet();
-            if (shuttingDown.get() && runningJobs.get() == 0) {
+            --runningJobs;
+            if (shuttingDown && runningJobs == 0) {
                 shutdownLock.notifyAll();
             }
         }
@@ -36,10 +33,10 @@ public class WaitForJobsOnShutdownJobListener extends JobListenerSupport {
 
     public void waitOnShutdownLock() throws InterruptedException {
         synchronized (shutdownLock) {
-            if (runningJobs.get() == 0) {
+            if (runningJobs == 0) {
                 return;
             }
-            shuttingDown.set(true);
+            shuttingDown = true;
             shutdownLock.wait();
         }
     }
